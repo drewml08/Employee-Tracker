5dcb83d7d60d7eb280017c93a66029a7f5c96e2e
Mike Drew <drewml08@gmail.com>
2021-07-22 01:42:30 -0400

    implemented add employee, added functions and promises for getting employee info and creating employees in database layer

--- src/Database.js
+++ src/Database.js
@@ -13,4 +13,59 @@ class Database {
             database: 'employee_db',
         });    
     }
+
+    getRoleChoices() {
+        return new Promise ( (resolve, reject) => {
+            this.connection.query('SELECT id, title FROM role', (err, res) => {
+                if (err) throw err;
+
+                var choices = [];   
+
+                for (var i = 0; i < res.length; i++) {
+                    choices.push({
+                        name: res[i].title, 
+                        value: res[i].id,
+                    });
+                } 
+                resolve(choices);
+            })
+        });
+    }
+
+    getEmployeeChoices() {
+        return new Promise ( (resolve, reject) => {
+            this.connection.query('SELECT id, first_name, last_name FROM employee', (err, res) => {
+                if (err) throw err;
+
+                var choices = [];   
+
+                for (var i = 0; i < res.length; i++) {
+                    choices.push({
+                        name: res[i].first_name + " " + res[i].last_name, 
+                        value: res[i].id,
+                    });
+                } 
+                resolve(choices);
+            })
+        });
+    }
+    createEmployee(employee) {
+        return new Promise ( (resolve, reject) => {
+            this.connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: employee.first_name,
+                    last_name: employee.last_name,
+                    role_id: employee.role,
+                    manager_id: employee.manager,
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`${res.affectedRows} employee created!\n`);
+                    resolve();
+            });
+        });
+    }
 }
+
+module.exports = Database;
--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
-
+const Database = require('./Database');
 
 
 class Menu {
     constructor(){
         this.done = false;
+        this.db = new Database();
     }
 
     mainPrompt() {
@@ -32,7 +33,7 @@ class Menu {
                     break;
 
                 case "Add Employee":
-                    this.addEmployee();
+                   await this.addEmployee();
                     break;
 
                 case "Update Employee Role":
@@ -48,9 +49,53 @@ class Menu {
     viewAllEmployees() {
 
     }
+    async addEmployeePrompt() {
+        const roles = await this.db.getRoleChoices();
+        var people = await this.db.getEmployeeChoices();
+        people.unshift({
+            name: 'none',
+            value: '0',
+        })
+        const question = [
+            {
+                type: 'input',
+                message: 'What is the employee\'s first name?',
+                name: 'first_name', 
+            },
+
+            {
+                type: 'input',
+                message: 'What is the employee\'s last name?',
+                name: 'last_name', 
+            },
+
+            {
+                type: 'list', 
+                message: 'What is the employee\'s role?',
+                choices: roles,
+                name: 'role',
+            },
+
+            {
+                type: 'list', 
+                message: 'Who is the employee\'s manager?',
+                choices: people,
+                name: 'manager',
+            }
+        ]
+        return new Promise ( (resolve, reject) => {
+            inquirer.prompt(question).then((response) => {
+                resolve(response);
+            });
+        });
+    }
 
     addEmployee() {
-
+        return new Promise (async (resolve, reject) => {
+            const employee = await this.addEmployeePrompt();
+            await this.db.createEmployee(employee);
+            resolve();
+        });
     }
     
     updateRole() {
